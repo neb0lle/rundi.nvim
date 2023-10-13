8b54ed9ebda0fb89ad857ea5fcd6b15ed67b149e
Amogh Kulkarni <65953251+amoghbk@users.noreply.github.com>
2023-10-14 02:52:05 +0530

    Initial test

--- lua/rundi/init.lua
+++ lua/rundi/init.lua
@@ -46,8 +46,52 @@ local function setup(user_config)
 	for filetype, options in pairs(config.autocompile) do
 		setup_autocompile(filetype, options)
 	end
+
+    vim.cmd([[command! Rundi :luado require("rundi").Rundi()]])
+    vim.cmd([[command! -nargs=* RundiSetFlag :luado require("rundi").RundiSetFlag(<f-args>)]])
+    vim.cmd([[command! -nargs=* RundiSetConfig :luado require("rundi").RundiSetConfig(<f-args>)]])
+end
+
+local function Rundi()
+    local filetype = vim.bo.filetype
+    local options = config.autocompile[filetype]
+
+    if options then
+        local compiler_args = options.compiler_args and (" " .. options.compiler_args) or ""
+        local output_flag = options.output_format and (" -o " .. options.output_format) or ""
+        local execute_flag = options.output_format and (" && ./" .. options.output_format) or ""
+        local input_file = ' "%"'
+
+        vim.cmd("split")
+        vim.cmd("te " .. options.compiler .. compiler_args .. input_file .. output_flag .. execute_flag)
+        vim.cmd("startinsert")
+    else
+        print("Autocompile options not found for this filetype")
+    end
+end
+
+local function RundiSetFlag(flags)
+    local filetype = vim.bo.filetype
+    local options = config.autocompile[filetype]
+
+    if options then
+        options.compiler_args = flags
+    else
+        print("Autocompile options not found for this filetype")
+    end
+end
+
+local function RundiSetConfig(new_config)
+    for key, value in pairs(new_config) do
+		if config[key] ~= nil then
+			config[key] = value
+		end
+	end
 end
 
 return {
 	setup = setup,
+    Rundi = Rundi,
+    RundiSetFlag = RundiSetFlag,
+    RundiSetConfig = RundiSetConfig
 }
